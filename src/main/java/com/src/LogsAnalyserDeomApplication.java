c8703ae3b323ea308e11f4b35c4f5690c030d795
anandpsuryavanshi <118662449+anandpsuryavanshi@users.noreply.github.com>
2022-11-20 19:25:05 +0530

    Update LogsAnalyserDeomApplication.java

--- src/main/java/com/src/LogsAnalyserDeomApplication.java
+++ src/main/java/com/src/LogsAnalyserDeomApplication.java
@@ -1,13 +1,34 @@
 package com.src;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.src.service.LogsService;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class LogsAnalyserDeomApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogsAnalyserDeomApplication.class);
 
+	@Autowired
+	private LogsService service;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(LogsAnalyserDeomApplication.class, args);
+		SpringApplication app = new SpringApplication(LogsAnalyserDeomApplication.class);
+		app.run(args);		
 	}
-
+	
+	 public void run(String... args) {
+	        Instant start = Instant.now();
+	        service.execute(args);
+	        Instant end = Instant.now();
+	        LOGGER.info("Total time: {}ms", Duration.between(start, end).toMillis());
+	 }
 }

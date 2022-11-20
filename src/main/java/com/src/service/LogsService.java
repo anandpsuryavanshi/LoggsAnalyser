5d1b6c28c6370e24b803bbc6184ac2a8809f12d7
anandpsuryavanshi <118662449+anandpsuryavanshi@users.noreply.github.com>
2022-11-20 19:24:19 +0530

    Update LogsService.java

--- src/main/java/com/src/service/LogsService.java
+++ src/main/java/com/src/service/LogsService.java
@@ -2,10 +2,28 @@ package com.src.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.src.manager.LogsManager;
+import com.src.model.Context;
+import com.src.validator.LogsValidator;
 
 @Service
 public class LogsService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(LogsService.class);
-	
+
+	@Autowired
+	private LogsValidator validator;
+
+	@Autowired
+	private LogsManager manager;
+
+	public void execute(String... args) {
+		Context context = Context.getInstance();
+		validator.validateInput(context, args);
+		manager.parseAndPersistEvents(context);
+	}
+
 }
+
